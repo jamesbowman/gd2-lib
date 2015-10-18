c82cf8b61b8eec8d9d97051f5dd47c5f17d5753f
jamesbowman <jamesb@excamera.com>
2015-10-18 10:45:10 -0700

    Add on-screen message

--- sketch.ino
+++ sketch.ino
@@ -8,9 +8,14 @@ void setup() //' a{
   GD.begin();
   GD.cmd_memset(0, 0, 480UL * 272UL);   // clear the bitmap
   GD.Clear();                           // draw the bitmap
+
+  GD.ColorRGB(0x101020);
+  GD.cmd_text(240, 136, 31, OPT_CENTER, "sketch demo");
+
   GD.BitmapLayout(L8, 480, 272);
   GD.BitmapSize(NEAREST, BORDER, BORDER, 480, 272);
   GD.Begin(BITMAPS);
+  GD.ColorRGB(0xffffff);
   GD.Vertex2ii(0, 0);
   GD.swap();
   GD.cmd_sketch(0, 0, 480, 272, 0, L8); // start sketching
