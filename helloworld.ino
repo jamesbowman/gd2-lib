ff7ac5ada94abce4ab47971e38ff1c02891d80c1
jamesbowman <jamesb@excamera.com>
2016-01-08 17:09:12 -0800

    Resolution independence

--- helloworld.ino
+++ helloworld.ino
@@ -12,6 +12,6 @@ void loop()
 {
   GD.ClearColorRGB(0x103000);
   GD.Clear();
-  GD.cmd_text(240, 136, 31, OPT_CENTER, "Hello world");
+  GD.cmd_text(GD.w / 2, GD.h / 2, 31, OPT_CENTER, "Hello world");
   GD.swap();
 } //' }A
