39618dd4f779ca0bce9bb720472eccc757b158db
James Bowman <jamesb@excamera.com>
2018-07-30 12:30:19 -0700

    SPIDriver port cleanup. Now runs contrib/life

--- pc/spidriver.cpp
+++ pc/spidriver.cpp
@@ -2,7 +2,6 @@
 #include <assert.h>
 #include <memory.h>
 #include <fcntl.h>
-#include <termios.h>
 #include <unistd.h>
 #include <errno.h>
 #define __STDC_FORMAT_MACROS
@@ -11,6 +10,104 @@
 #include "spidriver.h"
 
 // ****************************   Serial port  ********************************
+
+#if defined(WIN32)  // {
+
+#include <windows.h>
+
+void ErrorExit(LPTSTR lpszFunction) 
+{ 
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError(); 
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR) &lpMsgBuf,
+        0, NULL );
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
+    sprintf((LPTSTR)lpDisplayBuf, TEXT("%s failed with error %d:\n%s"), lpszFunction, dw, lpMsgBuf); 
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(dw); 
+}
+
+HANDLE openSerialPort(LPCSTR portname)
+{
+    DWORD  accessdirection = GENERIC_READ | GENERIC_WRITE;
+    HANDLE hSerial = CreateFile(portname,
+        accessdirection,
+        0,
+        0,
+        OPEN_EXISTING,
+        0,
+        0);
+    if (hSerial == INVALID_HANDLE_VALUE) {
+        ErrorExit("CreateFile");
+    }
+    DCB dcbSerialParams = {0};
+    dcbSerialParams.DCBlength=sizeof(dcbSerialParams);
+    if (!GetCommState(hSerial, &dcbSerialParams)) {
+         ErrorExit("GetCommState");
+    }
+    dcbSerialParams.BaudRate = 460800;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+    if (!SetCommState(hSerial, &dcbSerialParams)) {
+         ErrorExit("SetCommState");
+    }
+    COMMTIMEOUTS timeouts = {0};
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+    if (!SetCommTimeouts(hSerial, &timeouts)) {
+        ErrorExit("SetCommTimeouts");
+    }
+    return hSerial;
+}
+
+DWORD readFromSerialPort(HANDLE hSerial, char * buffer, int buffersize)
+{
+    DWORD dwBytesRead = 0;
+    if (!ReadFile(hSerial, buffer, buffersize, &dwBytesRead, NULL)) {
+        ErrorExit("ReadFile");
+    }
+    return dwBytesRead;
+}
+
+DWORD writeToSerialPort(HANDLE hSerial, const char * data, int length)
+{
+    DWORD dwBytesRead = 0;
+    if (!WriteFile(hSerial, data, length, &dwBytesRead, NULL)) {
+        ErrorExit("WriteFile");
+    }
+    return dwBytesRead;
+}
+
+void closeSerialPort(HANDLE hSerial)
+{
+    CloseHandle(hSerial);
+}
+
+#else               // }{
+
+#include <termios.h>
 
 int openSerialPort(const char *portname)
 {
@@ -72,6 +169,7 @@ void writeToSerialPort(int fd, const char *b, size_t s)
   printf("\n");
 #endif
 }
+#endif              // }
 
 // ******************************  SPIDriver  *********************************
 
@@ -79,14 +177,17 @@ void spi_connect(SPIDriver *sd, const char* portname)
 {
   sd->port = openSerialPort(portname);
   writeToSerialPort(sd->port, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", 64);
-  for (int i = 0; i < 256; i++) {
-    char tx[2] = {'e', (char)i};
+
+  const char tests[] = "A\r\n\0xff";
+  for (int i = 0; i < 4; i++) {
+    char tx[2] = {'e', tests[i]};
     writeToSerialPort(sd->port, tx, 2);
     char rx[1];
-    readFromSerialPort(sd->port, rx, 1);
-    if ((rx[0] & 0xff) != i)
+    int n = readFromSerialPort(sd->port, rx, 1);
+    if (rx[0] != tests[i])
       assert(0);
   }
+
   printf("\n");
   spi_getstatus(sd);
 }
